6882a6fe355019ea10476360e85fd1511d8349a1
Margaeth <margarth.aliendres@gmil.com>
2022-12-07 17:43:37 -0300

    Dio

---
+++ JAVA-START CAPGEMINI/Estudo/app/src/main/java/Estudo/variaveiseOperadores.java
@@ -0,0 +1,43 @@
+
+package Estudo;
+
+public class variaveiseOperadores {
+    
+
+    public static void main(String[] args) {
+        
+        int marg = 10;
+        System.out.println(marg);
+                
+        marg = 20;
+        System.out.println(marg);
+        
+        int marga = 10 + 10;
+        System.out.println(marga);
+        
+        
+        final float pi = (float) 3.14;
+       
+        System.out.println(pi);
+        
+        
+        float combustivel = 5.79f;
+        int kmGastoPlitro = 10;
+        int kmviagem = 100;
+        
+        int totalKmgasto = kmviagem/kmGastoPlitro;
+        float totalgasto = totalKmgasto*combustivel;
+        
+        System.out.println("Total gasto é = " + totalgasto);
+        
+        
+        
+        
+        
+        
+  
+        
+        
+        
+    }
+}
---
+++ REACT-DIO/projetoreact1/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      Aleluia Te amooooooooooo Deus, cuida da minha familia. Coloco todos na palma de suas maos.
+    </div>
+  );
+}
+
+export default App;
